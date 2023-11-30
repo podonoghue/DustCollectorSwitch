4002dd72f0a6b3b7811607ae1e38cb62cee896e3
Peter O'Donoghue <1848579+podonoghue@users.noreply.github.com>
2023-11-30 14:43:10 +1100

    Version 4 - Minor tweaks

---
+++ Software/DustCollector_V4_KE04Z4/Release/Sources/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Sources/delay.cpp \
+../Sources/hardware.cpp \
+../Sources/i2c.cpp \
+../Sources/main.cpp \
+../Sources/spi.cpp \
+../Sources/usbdmError.cpp 
+
+CPP_DEPS += \
+./Sources/delay.d \
+./Sources/hardware.d \
+./Sources/i2c.d \
+./Sources/main.d \
+./Sources/spi.d \
+./Sources/usbdmError.d 
+
+OBJS += \
+./Sources/delay.o \
+./Sources/hardware.o \
+./Sources/i2c.o \
+./Sources/main.o \
+./Sources/spi.o \
+./Sources/usbdmError.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Sources/%.o: ../Sources/%.cpp Sources/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM C++ Compiler'
+	arm-none-eabi-g++ -mcpu=cortex-m0plus -mthumb -fcallgraph-info=su -O3 -ffunction-sections -fdata-sections -fno-rtti -Wall -Wextra -DRELEASE_BUILD -DNDEBUG -I"../Sources" -I"../Project_Headers" -fno-exceptions -std=c++20 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@"  -o "$@" $<
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-Sources
+
+clean-Sources:
+	-$(RM) ./Sources/delay.d ./Sources/delay.o ./Sources/hardware.d ./Sources/hardware.o ./Sources/i2c.d ./Sources/i2c.o ./Sources/main.d ./Sources/main.o ./Sources/spi.d ./Sources/spi.o ./Sources/usbdmError.d ./Sources/usbdmError.o
+
+.PHONY: clean-Sources
+
---
+++ Software/DustCollector_V4_KE04Z4/Release/sources.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ASM_SRCS := 
+C++M_SRCS := 
+C++_SRCS := 
+CCM_SRCS := 
+CC_SRCS := 
+CPP_SRCS := 
+CXXM_SRCS := 
+CXX_SRCS := 
+C_SRCS := 
+C_UPPER_SRCS := 
+ELF_SRCS := 
+EXE_SRCS := 
+OBJ_SRCS := 
+OUT_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+ARCHIVES := 
+C++M_DEPS := 
+C++_DEPS := 
+CCM_DEPS := 
+CC_DEPS := 
+CPP_DEPS := 
+CXXM_DEPS := 
+CXX_DEPS := 
+C_DEPS := 
+C_UPPER_DEPS := 
+EXECUTABLES := 
+LIST_FILE_OUTPUTS := 
+MAP_FILE_OUTPUTS := 
+OBJS := 
+SIZE_FILE_OUTPUTS := 
+SYMBOL_LIST_FILE_OUTPUTS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Sources \
+Startup_Code \
+
